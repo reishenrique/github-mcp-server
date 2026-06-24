b6d5b93f32793386482fe2488077b5c9afe7099d
Henrique Reis <contatohenriquereis@gmail.com>
2026-06-24 16:43:03 -0300

    feat: adding another cache map (for now)

--- src/cache/summary-pull-request.cache.ts
+++
@@ -1,8 +0,0 @@
-import { FormatSummarizePullRequestOutput } from '../types/summarize-pull-request.type.js';
-
-export type SummarizePullRequestCacheValue = {
-  summary: FormatSummarizePullRequestOutput;
-  generatedAt: string;
-};
-
-export const summaryPullRequestCache = new Map<string, SummarizePullRequestCacheValue>();
