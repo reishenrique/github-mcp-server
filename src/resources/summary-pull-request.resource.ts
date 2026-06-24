2a69373e58d8f1725e033ccf7c1c66a26c574e5a
Henrique Reis <contatohenriquereis@gmail.com>
2026-06-24 17:40:29 -0300

    fix: adjusting the pull request summary resource logic by removing ambiguity and unnecessary elements that the function being used now already handles

--- src/resources/summary-pull-request.resource.ts
+++ src/resources/summary-pull-request.resource.ts
@@ -1,6 +1,5 @@
 import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp';
-import { getCachedSummaryPullRequest } from '../services/pull-requests.service.js';
-import { buildSummaryPullRequestCacheKey } from '../utils/builders/build-pull-request-cache-keys.util.js';
+import { getOrCreateSummarizePullRequest } from '../services/pull-requests.service.js';
 import { formatPullRequestSummaryResource } from '../formatters/summarize-pull-request-resource.formatter.js';
 
 export function registerSummaryPullRequestResource(server: McpServer) {
@@ -17,23 +16,19 @@ export function registerSummaryPullRequestResource(server: McpServer) {
         'Provides a structured pull request summary resource after they have been generated once by the tool',
     },
     async (uri: any, variables: any) => {
-      const cacheKey = buildSummaryPullRequestCacheKey(
+      const summary = await getOrCreateSummarizePullRequest(
         variables.owner,
         variables.repositoryName,
         variables.pullRequestNumber,
       );
 
-      const cachedSummary = getCachedSummaryPullRequest(cacheKey);
-
-      if (!cachedSummary) throw new Error('Pull request summary not generated yet');
-
-      const formatSummaryToComprehensionContext = formatPullRequestSummaryResource(cachedSummary);
+      const generatedAt = new Date().toISOString();
 
       return {
         contents: [
           {
             uri: uri.href,
-            text: formatSummaryToComprehensionContext,
+            text: formatPullRequestSummaryResource({ summary, generatedAt }),
           },
         ],
       };
