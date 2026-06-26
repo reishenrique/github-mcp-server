2e7e8e118b13b7f33645bed6026e6f12c882db7d
Henrique Reis <contatohenriquereis@gmail.com>
2026-06-26 16:58:16 -0300

    feat: using an environment varible for the github api url

--- src/config/github.config.ts
+++ src/config/github.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import 'dotenv/config';
 
 export const gitHubApi = axios.create({
-  baseURL: 'https://api.github.com',
+  baseURL: process.env.GITHUB_API_URL,
   headers: {
     Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
     Accept: 'application/vnd.github+json',
