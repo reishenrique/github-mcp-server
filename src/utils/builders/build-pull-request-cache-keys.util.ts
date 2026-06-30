db01e11549bb6b8a633ea4672c75b01c9218e2fa
Henrique Reis <contatohenriquereis@gmail.com>
2026-06-30 15:13:40 -0300

    feat: builder implementation responsible for creating a cache key for the review checklist

--- src/utils/builders/build-pull-request-cache-keys.util.ts
+++ src/utils/builders/build-pull-request-cache-keys.util.ts
@@ -13,3 +13,11 @@ export function buildPullRequestReviewContextCacheKey(
 ): string {
   return `reviewContext:${owner}-${repositoryName}-${pullRequestNumber}`;
 }
+
+export function buildPullRequestReviewChecklistCacheKey(
+  owner: string,
+  repositoryName: string,
+  pullRequestNumber: number,
+): string {
+  return `reviewChecklsit:${owner}-${repositoryName}-${pullRequestNumber}`;
+}
